678395c5c98cecb2e5b09fc5dfb6db48f4ffd9f1
Amal K <1c2756149+amalk-au@users.noreply.github.com>
2026-09-15 07:59:38 +1000

    Removed webpack, jest, babel and other config files

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // Other Jest configuration options
-    // Following will direct jest to a Mock styleMock.js so Jest doesn't throw an error by parsing CSS files
-    moduleNameMapper: {
-      '\\.(css|less|sass|scss)$': '<rootDir>/src/style-mock.js',
-    },
-  };
